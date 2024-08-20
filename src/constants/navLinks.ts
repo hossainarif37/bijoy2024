871d70df47db2026dc9fa0363035f2536f9e36ec
hossainarif37 <hossainarif4937@gmail.com>
2024-08-21 05:12:33 +0600

    feat: Setup initially with Responsive Navbar

---
+++ src/constants/navLinks.ts
@@ -0,0 +1,14 @@
+export const navLinks = [
+    {
+        path: '/',
+        title: 'Home'
+    },
+    {
+        path: '/login',
+        title: 'Login'
+    },
+    {
+        path: '/register',
+        title: 'Register'
+    }
+]
---
+++ src/shared/Navbar/Navbar.tsx
@@ -0,0 +1,42 @@
+"use client"
+import { navLinks } from "@/constants/navLinks";
+import Link from "next/link";
+import { useState } from "react";
+import { Icon } from "@iconify/react";
+
+const Navbar = () => {
+    const [isNavToggeled, setIsNavToggled] = useState(false);
+    return (
+        <nav className="flex justify-between item-center max-w-7xl px-2 md:px-0 mx-auto py-5">
+            <h1 className="text-xl md:text-3xl font-extrabold">
+                <span className="text-success">Bijoy</span><span className="text-error">2024</span>
+            </h1>
+            {/* Desktop */}
+            <ul className="hidden md:flex gap-x-14">
+                {
+                    navLinks.map(link => (
+                        <li className="text-lg font-semibold text-black-100" key={link.path}><Link href={link.path}>{link.title}</Link></li>
+                    ))
+                }
+            </ul>
+
+            {/* Mobile */}
+            <div className="block md:hidden relative">
+                <button onClick={() => setIsNavToggled(prev => !prev)}>
+                    <span className="text-3xl"><Icon icon={`material-symbols:${isNavToggeled ? 'close' : 'menu'}`} /></span>
+                </button>
+                <ul className={`${isNavToggeled ? "scale-y-100" : "scale-y-0"} absolute top-6 right-0 bg-white shadow-lg space-y-2 p-5 rounded-xl w-48 duration-200 origin-top`}>
+                    {
+                        navLinks.map(link => (
+                            <li key={link.path}><Link href={link.path}>{link.title}</Link></li>
+                        ))
+                    }
+                </ul>
+
+            </div>
+
+        </nav>
+    );
+};
+
+export default Navbar;
