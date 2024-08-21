e25f5111e90e827bdf7ae759018b31cb29d66ee2
hossainarif37 <hossainarif4937@gmail.com>
2024-08-21 17:15:38 +0600

    feat: Design Google Button

--- src/app/(auth)/login/component/Login.tsx
+++ src/app/(auth)/login/component/Login.tsx
@@ -1,8 +1,10 @@
 "use client"
 
 import { Button, ConfigProvider, Input, Space } from "antd";
+import Image from "next/image";
 import Link from "next/link";
 import { useState } from "react";
+
 
 
 const Login = () => {
@@ -34,13 +36,25 @@ const Login = () => {
                 </div>
 
                 {/* Submit Button */}
-                <div className="flex flex-col gap-y-2">
+                <div className="flex flex-col">
                     <button type="submit" className="btn btn-primary">Login</button>
 
-                    <p className="flex justify-center gap-x-2">
+                    <p className="flex justify-end gap-x-2 mt-3">
                         <span>Don't have an account?</span> <Link href={'/register'} className="underline text-blue-600">Register</Link>
                     </p>
+
                 </div>
+
+                    {/* Separator */}
+                    <div className="w-[88%] mx-auto h-[1px] bg-gray-300 opacity-50"></div>
+                <div>
+                    <button type="button" className="btn border flex justify-center gap-x-5 item-center text-black-100">
+                        <Image src={'/google_icon.png'} alt="Google Icon" width={24} height={24} />
+
+                        <span>Continue with google</span>
+                    </button>
+                </div>
+
             </div>
         </form>
     );
