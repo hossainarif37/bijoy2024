596b205cbcd9852de83319eb141e104b62ef7e60
hossainarif37 <hossainarif4937@gmail.com>
2024-08-21 22:07:28 +0600

    feat(Loading): Update the Loading component with custom spinner

--- src/components/ui/Loading.tsx
+++ src/components/ui/Loading.tsx
@@ -2,7 +2,7 @@
 const Loading = () => {
     return (
         <div className="h-screen flex justify-center py-40">
-            Loading...
+            <span className="w-16 h-16 animate-spin border-t-4 border-primary rounded-full"></span>
         </div>
     );
 };
